71506bf1c242b5abeeaad57c22b17b3b77482e98
Emre <esokullu@gmail.com>
2019-04-20 13:24:00 -0700

    language edit

--- app/pages/Comparing.jsx
+++ app/pages/Comparing.jsx
@@ -81,7 +81,7 @@ export default class Comparing extends React.Component {
             <Main id='comparing' data-page='comparing'>
                 <Hero>
                     <Animation animationConfig={animationConfig} />
-                    <Headline title="Grou.ps vs Facebook Groups" subtitle="Check out our comparison" />
+                    <Headline title="Grou.ps vs Facebook Groups" subtitle="Why pay $8/month, right?" />
                 </Hero>
                 <main>
                     <section id="comparing-table">
