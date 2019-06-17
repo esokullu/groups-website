92f9169ed6c0aa8f0bc7f31c017fcd5b7dc4c499
Emre <esokullu@gmail.com>
2019-06-17 13:19:26 -0700

    removed isHouseAccount

--- app/scripts/apiPaths.js
+++ app/scripts/apiPaths.js
@@ -1,7 +1,6 @@
 const apiWeb = 'https://www.phonetworks.com/api';
-const apiGjs = 'https://build.phonetworks.com';
 const apiSandbox = 'https://gjs-sandbox.herokuapp.com/api';
 
 export {
-    apiWeb, apiGjs, apiSandbox
+    apiWeb, apiSandbox
 };
--- app/scripts/requestGjs.js
+++ app/scripts/requestGjs.js
@@ -1,8 +1,8 @@
-import {apiGjs} from './apiPaths.js';
+import {apiWeb} from './apiPaths.js';
 
 export default function(path, data, uuid, graphjsHash, callback) {
 	// https://stackoverflow.com/questions/14525178/is-there-any-native-function-to-convert-json-to-url-parameters
-	let _apiGjs = apiGjs
+	let _apiGjs = apiWeb;
     let target = _apiGjs+'/'+path+'?hash='+graphjsHash+'&public_id='+uuid+'&'+Object.entries(data).map(e => e.join('=')).join('&');
 	let parameters = {
 	    method: "GET",
