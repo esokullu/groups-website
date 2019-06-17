1704b0ff05edeae0a7c3ff1b1b116f494f84e28f
Emre <esokullu@gmail.com>
2019-06-17 13:23:54 -0700

    removed isHouseAccount

--- app/scripts/requestGjs.js
+++ app/scripts/requestGjs.js
@@ -2,7 +2,7 @@ import {apiWeb} from './apiPaths.js';
 
 export default function(path, data, uuid, graphjsHash, callback) {
 	// https://stackoverflow.com/questions/14525178/is-there-any-native-function-to-convert-json-to-url-parameters
-	let _apiGjs = apiWeb;
+	let _apiGjs = apiGjs;
     let target = _apiGjs+''+path+'?hash='+graphjsHash+'&public_id='+uuid+'&'+Object.entries(data).map(e => e.join('=')).join('&');
 	let parameters = {
 	    method: "GET",
